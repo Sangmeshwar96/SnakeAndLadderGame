258e32080184ce7dbf49a5c8be52f0b84f28000c
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-05 18:23:22 +0530

    Welcome To Snake And Ladder Game Program

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Xml.Linq;
+using SnakeLadder;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Game");
+            Snake snake = new Snake();
+            snake.Ladder();
+
+        }
+    }
+}
