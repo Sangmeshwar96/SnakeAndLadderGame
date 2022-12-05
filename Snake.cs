cf9c69b70de8694121d8a8fa0d389fcf17aa6d17
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-05 18:27:30 +0530

    Dice Rolled For Player 1

--- Snake.cs
+++ Snake.cs
@@ -13,6 +13,9 @@ namespace SnakeLadder
         {
             int position = 0;
             Console.WriteLine("The position of player is : " +position);
+            Random random = new Random();
+            int dice = random.Next(1, 7);
+            Console.WriteLine("The number got by dice roll is : " + dice);
         }
     }
 }
