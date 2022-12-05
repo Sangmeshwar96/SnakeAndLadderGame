476358def2afdd592cf111c458707e24545ce31a
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-05 18:42:52 +0530

    Repeating Till Player Reaches Winning Position 100

--- Snake.cs
+++ Snake.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,10 +8,44 @@ namespace SnakeLadder
 {
     public class Snake
     {
+        public const int ladder = 1, snake = 2;
         public void Ladder()
         {
-            int position = 0;
-            Console.WriteLine("The position of player is : " +position);
+            int position1 = 0;
+            Console.WriteLine("The starting position of player is : " + position1);
+            Console.WriteLine(" ");
+            do
+            {
+                Random random = new Random();
+                int dice = random.Next(1, 7);
+                Console.WriteLine("The number got by dice roll is : " +dice);
+                Random random2 = new Random();
+                int option = random2.Next(0, 3);
+                switch (option)
+                {
+                    case ladder:
+                        position1 += dice;
+                        Console.WriteLine("The player climbed the ladder");
+                        break;
+                    case snake:
+                        position1 -= dice;
+                        if (position1 < 0)
+                        {
+                            Console.WriteLine("The player bit by snake");
+                            position1 = 0;
+                        }
+                        else
+                        {
+                            Console.WriteLine("The player bit by snake");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("No play");
+                        break;
+                }
+                Console.WriteLine("The player position is : " +position1);
+                Console.WriteLine(" ");
+            } while (position1 < 100);
         }
     }
 }
